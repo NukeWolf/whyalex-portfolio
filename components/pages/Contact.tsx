8a4818e2ab6a355b85317e2870011d52f77fa3f5
NukeWolf <smart.alex.huang@gmail.com>
2024-08-12 00:02:44 -0400

    contact page form finished with api endpoint.

--- components/pages/Contact.tsx
+++ components/pages/Contact.tsx
@@ -18,7 +18,46 @@ function ContactBody() {
     email: "",
     message: "",
   });
-  const handleSubmit = () => {};
+
+  const [showSuccess, setSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setSuccess(false);
+  };
+  const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+    try {
+      console.log("help");
+      const params = new URLSearchParams(formData).toString();
+      const response = await fetch(
+        `${import.meta.env.VITE_GOOGLE_API_ENDPOINT}?${params}`,
+        {
+          redirect: "follow",
+          headers: {
+            "Content-Type": "text/plain;charset=utf-8",
+          },
+        }
+      );
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Network Error");
+      }
+      setFormData({ name: "", email: "", message: "" });
+      alert(
+        "Form submitted successfully! You should receive a confirmation email."
+      );
+      setSuccess(true);
+    } catch (error) {
+      console.log(error);
+      alert("Error on form submission, please submit again.");
+    }
+  };
+
   return (
     <div className={styles["contact-panel"]}>
       <div className={styles["hero-card"]}>
@@ -34,8 +73,17 @@ function ContactBody() {
         </div>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" value={formData.name} />
+            <label htmlFor="name">
+              Name:
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </label>
           </div>
 
           <div>
@@ -45,15 +93,24 @@ function ContactBody() {
               id="email"
               name="email"
               value={formData.email}
+              onChange={handleChange}
+              required
             />
           </div>
 
           <div>
             <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" value={formData.message} />
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
           </div>
 
           <button type="submit">Submit</button>
+          {showSuccess && <p>Thanks for reaching out! I'll reach out soon!</p>}
         </form>
       </div>
     </div>
